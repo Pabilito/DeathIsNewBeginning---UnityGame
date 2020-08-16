6ff008ac5e189ed8042836d12c10858da02dc7f5
Pabilito <40524000+Pabilito@users.noreply.github.com>
2020-08-16 18:38:43 +0200

    Level 0_3 preparation

---
+++ Assets/Scripts/LevelsManagers/Levels0/Level0_3/TextHandler0_3.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextHandler0_3 : MonoBehaviour
+{
+    public Text tutorialTxt;
+
+    void Start()
+    {
+        tutorialTxt.GetComponent<Text>().text = "To be done";
+    }
+
+    private void Update()
+    {
+        if (this.GetComponent<HealthAndPointsManager>().getHealth() < 0)
+        {
+            tutorialTxt.GetComponent<Text>().text = "";
+        }
+        else if (this.GetComponent<HealthAndPointsManager>().getHealth() == 0)
+        {
+            tutorialTxt.GetComponent<Text>().text = "";
+        }
+        else if (this.transform.position.x > -2)
+        {
+            tutorialTxt.GetComponent<Text>().text = "";
+        }
+        else
+        {
+            tutorialTxt.GetComponent<Text>().text = "Level in development";
+        }
+    }
+
+}
