c79c73d04acbef4d69271303e29a961c65e4fc36
Pabilito <40524000+Pabilito@users.noreply.github.com>
2020-08-18 22:05:45 +0200

    Video ad

---
+++ Assets/Scripts/ADS/ADS.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class ADS : MonoBehaviour
+{
+    string gameId = "3769625";
+    string bannerID = "Banner1";
+    bool testMode = false;
+
+    void Start()
+    {
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        Advertisement.Initialize(gameId, testMode);
+        StartCoroutine(ShowBannerWhenInitialized());
+    }
+
+    IEnumerator ShowBannerWhenInitialized()
+    {
+        while (!Advertisement.isInitialized)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.Show(bannerID);
+    }
+}
---
+++ Assets/Scripts/ADS/ADSlevels0.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class ADSlevels0 : MonoBehaviour
+{
+    string videoID = "video";
+
+    void Start()
+    {
+        StartCoroutine(ShowVideoWhenInitialized());
+    }
+
+    IEnumerator ShowVideoWhenInitialized()
+    {
+        while (!Advertisement.IsReady(videoID))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Show(videoID);
+    }
+}
