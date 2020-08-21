fe23fe404a6a6bb9188d7f42a954e52fefabdcc9
Pabilito <40524000+Pabilito@users.noreply.github.com>
2020-08-21 11:23:28 +0200

    movement update

--- Assets/Scripts/MiniGame1/PlayerMovement.cs
+++ Assets/Scripts/MiniGame1/PlayerMovement.cs
@@ -44,14 +44,7 @@ public class PlayerMovement : MonoBehaviour
 
             Vector3 dir = Vector3.zero;
 
-            // we assume that device is held parallel to the ground
-            // and Home button is in the right hand
-
-            // remap device acceleration axis to game coordinates:
-            //  1) XY plane of the device is mapped onto XZ plane
-            //  2) rotated 90 degrees around Y axis
-            dir.x = -Input.acceleration.y;
-            dir.z = Input.acceleration.x;
+            dir.x = Input.acceleration.x;
 
             // clamp acceleration vector to unit sphere
             if (dir.sqrMagnitude > 1)
