a260a4b92b89ad18bd04dd2698a544f4a94538d9
Pabilito <40524000+Pabilito@users.noreply.github.com>
2020-08-21 11:58:40 +0200

    Level 0_3 finished

--- Assets/Scripts/LevelsManagers/Levels0/Level0_3/WinHandler0_3.cs
+++ Assets/Scripts/LevelsManagers/Levels0/Level0_3/WinHandler0_3.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class WinHandler0_3 : MonoBehaviour
 {
+    public GameObject player;
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (GameObject.FindGameObjectWithTag("Player").GetComponent<HealthAndPointsManager>().getPoints() > 0)
+        if (player.GetComponent<HealthAndPointsManager>().getPoints() > 0)
         {
             GlobalVariables.instance.setLevelHighScore(3, 1);
         }
