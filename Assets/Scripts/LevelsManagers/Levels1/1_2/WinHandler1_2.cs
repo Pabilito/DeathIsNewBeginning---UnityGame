8fcecde76e32f98601732930f7a16c43e72233bc
Pabilito <40524000+Pabilito@users.noreply.github.com>
2020-08-22 11:10:03 +0200

    Build 0.0.6

---
+++ Assets/Scripts/LevelsManagers/Levels1/1_2/WinHandler1_2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinHandler1_2 : MonoBehaviour
+{
+    public GameObject player;
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        int points = player.GetComponent<HealthAndPointsManager>().getPoints();
+        if (points > 0)
+        {
+            GlobalVariables.instance.setLevelHighScore(12, points);
+        }
+    }
+}
