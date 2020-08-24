90f8d7fcb0f9de518c17fe0741f29a467396026e
Pabilito <40524000+Pabilito@users.noreply.github.com>
2020-08-24 12:37:32 +0200

    build 0.0.11

---
+++ Assets/Scripts/LevelsManagers/Levels1/1_8/randomizer1_8.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomizer1_8 : MonoBehaviour
+{
+    public GameObject point;
+    public GameObject obstacle;
+
+    void Start()
+    {
+        int x = Random.Range(0, 2);
+        if (x == 0)
+        {
+            Vector2 position;
+            position = point.transform.position;
+            point.transform.position = obstacle.transform.position;
+            obstacle.transform.position = position;
+        }
+    }
+
+}
