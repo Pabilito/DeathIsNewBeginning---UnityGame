0755a9122ea578caf8885904caf7eceef42e6129
Pabilito <40524000+Pabilito@users.noreply.github.com>
2020-09-06 19:39:09 +0200

    build 0.1.6

--- Assets/Scripts/MiniGame1/TriggerBoost.cs
+++ Assets/Scripts/MiniGame1/TriggerBoost.cs
@@ -6,6 +6,7 @@ public class TriggerBoost : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D col)
     {
-        col.GetComponent<Rigidbody2D>().velocity = Vector2.up * 50.0f * Mathf.Cos(this.transform.eulerAngles.z) + Vector2.left * 50.0f * Mathf.Sin(this.transform.eulerAngles.z);
+        col.GetComponent<Rigidbody2D>().velocity = Vector2.up * 50.0f * Mathf.Cos(this.transform.eulerAngles.z * Mathf.PI / 180) + Vector2.left * 50.0f * Mathf.Sin(this.transform.eulerAngles.z * Mathf.PI / 180);
+        print((this.transform.eulerAngles.z).ToString());
     }
 }
